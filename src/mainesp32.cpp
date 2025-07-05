f1c01c9d863ac031c061eb5e96def30d9038d16a
Pavlokruz <pavlokruz@gmail.com>
2025-07-04 21:18:21 -0300

    estos son todos los archivos que configunan un controlador Esp32, dos leds y un rele

---
+++ src/mainesp32.cpp
@@ -0,0 +1,21 @@
+#include <Arduino.h>
+
+#define LED_PIN 2
+#define RELAY_PIN 4
+
+void setup() {
+  Serial.begin(115200);       // Inicia la comunicación serie a 115200 baudios
+  pinMode(LED_PIN, OUTPUT);   // Configura el pin 2 como salida
+  pinMode(RELAY_PIN, OUTPUT); // Configura el pin 4 como salida
+}
+
+void loop() {
+  digitalWrite(LED_PIN, HIGH);   // Enciende el LED
+  digitalWrite(RELAY_PIN, HIGH); // Activa el relay
+  Serial.println("LED encendido, Relay activado");
+  delay(500);
+  digitalWrite(LED_PIN, LOW);   // Apaga el LED
+  digitalWrite(RELAY_PIN, LOW); // Desactiva el relay
+  Serial.println("LED apagado, Relay desactivado");
+  delay(500);
+}
